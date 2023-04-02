1cd2100207020882242679b82b9398630a6650c9
Arjun Pandey <54829402+parjun8840@users.noreply.github.com>
2023-04-02 11:47:31 +0900

    Update send_slack.sh

--- scripts/send_slack.sh
+++ scripts/send_slack.sh
@@ -29,4 +29,19 @@ fi
 [ -z "${workflow}" ] && workflow="TBD"
 env
 send_slack
+inputs='{"ref": "main", "inputs": {"text": "'$text'"}}'
+api_url=https://api.github.com/repos/parjun8840/gha01/actions/workflows/$workflow_id/dispatches
+response=$(curl -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $PERSONAL_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    $api_url \
+    -d "$inputs" 2>/dev/null )
+
+status_code=$?
+
+if [ $status_code -ne 0 ]; then
+  echo "Github Action- Slack send notification failed"
+  exit 1
+fi
 
